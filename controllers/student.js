7b3de241a051e2f2cbace726c6eb78ddf9acc738
Ankush Ladani <astarladani@gmail.com>
2023-02-21 16:16:33 +0530

    Done with Register a User and also added BigPromise Middleware to avoid try catch blocks. And also added callingCodeSchema which will be used in contact

--- controllers/student.js
+++ controllers/student.js
@@ -1,5 +1,41 @@
-const registerStudent = (req, res, next) => {
-  res.send('Registered Successfully');
-};
+const bcrypt = require('bcryptjs');
+const BigPromise = require('../middlewares/BigPromise.js');
+const User = require('../models/userSchema.js');
+
+const registerStudent = BigPromise(async (req, res, next) => {
+  // TODO : What to do with Contact?
+  let { name, email, department, role, id, password, confirmPassword } = req.body;
+
+  if (!name || !email || !department || !role || !id || !password || !confirmPassword) {
+    res.status(400).json({
+      message: 'Enter All the Required Details...',
+    });
+  }
+
+  if (password != confirmPassword) {
+    res.status(404).json({
+      message: 'Passwords do not match!!',
+    });
+  }
+  password = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    name,
+    email,
+    password,
+    id,
+    role,
+    department,
+  });
+
+  user.save((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        err: err.message,
+      });
+    }
+    return res.status(201).json(user);
+  });
+});
 
 module.exports = { registerStudent };
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,70 +1,66 @@
-const { checkSchema } = require('express-validator');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 const userSchema = new mongoose.Schema(
-  checkSchema(
-    {
-      name: {
-        type: String,
-        required: true,
-        maxLength: 22,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        maxLength: 20,
-        isEmail: {
-          bail: true,
-          errorMessage: 'Invalid Email',
-        },
-      },
-      phone: {
-        type: Number,
-        maxLength: 12,
-        trim: true,
-        isInt: {
-          errorMessage: 'Invalid Phone Number',
-          bail: true,
-        },
-      },
-      department: {
-        type: String,
-        enum: ['CH', 'CE', 'IT', 'MH', 'CL', 'EC', 'EE'],
-        required: true,
-      },
-      role: {
-        type: String,
-        enum: ['STUDENT', 'TEACHER'],
-        required: true,
-      },
-      uniqueId: {
-        type: Number,
-        unique: true,
-        required: true,
-      },
-      // TODO -  WE HAVE TO DECIDE SHOULD WE WANT TO SAVE PASSWORD IN DB OR NOT?
-      password: {
-        type: String,
-        required: true,
-        maxLength: 16,
-        isAscii: {
-          errorMessage: 'Enter valid Password',
-        },
-      },
-      confirmPassword: {
-        type: String,
-        required: true,
-        maxLength: 16,
-        isAscii: {
-          errorMessage: 'Enter valid Password',
-        },
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: 22,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [validator.isEmail, 'Provide valid Email Address'],
+    },
+    contact: {
+      type: Number,
+      maxLength: 12,
+      trim: true,
+      validate: [validator.isInt, 'Provide Numbers Only'],
+      callingCode: {
+        type: mongoose.ObjectId,
+        ref: 'CallingCode',
       },
     },
-    { timestamps: true },
-  ),
+    department: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['STUDENT', 'TEACHER'],
+      required: true,
+    },
+    id: {
+      type: Number,
+      unique: true,
+      required: true,
+    },
+    // TODO -  WE HAVE TO DECIDE SHOULD WE WANT TO SAVE PASSWORD IN DB OR NOT?
+    // * DONE
+    password: {
+      type: String,
+      required: true,
+      validate: [validator.isAscii, 'Provide Valid Password'],
+    },
+    confirmPassword: {
+      type: String,
+      validate: [validator.isAscii, 'Provide Valid Password'],
+    },
+  },
+  { timestamps: true },
 );
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified(this.password)) {
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
