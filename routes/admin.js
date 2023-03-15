8bc5fcb326cd2be035b00d8fc076d7a5373f770c
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-03-15 13:47:02 +0530

    add local

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+require('../database/connection');
+var userSchema = require('../model/userSchema');
+
+router.post('/register', async (req, res) => {});
+
 module.exports = router;
