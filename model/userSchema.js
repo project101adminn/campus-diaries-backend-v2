0245d81033fca0baf400f19af34dafcd06af09b6
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-03-21 23:09:16 +0530

    token generated for authentication

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,4 +1,5 @@
 const mongooose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongooose.Schema({
@@ -22,19 +23,33 @@ const userSchema = new mongooose.Schema({
     type: String,
     required: true,
   },
-  cpassword: {
-    type: String,
-    required: true,
-  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 12);
-    this.cpassword = await bcrypt.hash(this.cpassword, 12);
   }
   next();
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const User = mongooose.model('USER', userSchema);
 
